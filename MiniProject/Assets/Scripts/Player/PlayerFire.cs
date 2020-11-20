f09458617e4408ff6110c8d1724544ac775c0e52
안영훈 <wsxc9468@gmail.com>
2020-11-20 14:36:36 +0900

    안드로이드 빌드 완료

--- MiniProject/Assets/Scripts/Player/PlayerFire.cs
+++ MiniProject/Assets/Scripts/Player/PlayerFire.cs
@@ -6,24 +6,45 @@ public class PlayerFire : MonoBehaviour
 {
     public GameObject bulletFactory;
     public Transform firePoint;
-
+    public float delay = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(FireCorutine());
     }
 
     // Update is called once per frame
     void Update()
     {
-        Fire();
+        //Fire();
     }
     private void Fire()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (AtkButton.Instance.isTouch == true)
         {
+
             GameObject bullet = Instantiate(bulletFactory);
             bullet.transform.position = firePoint.position;
+        }
+        //if (Input.GetButtonDown("Fire1"))
+        //{
+        //    GameObject bullet = Instantiate(bulletFactory);
+        //    bullet.transform.position = firePoint.position;
+        //}
+    }
+    IEnumerator FireCorutine()
+    {
+        while (true)
+        {
+            if (AtkButton.Instance.isTouch == true)
+            {
+
+                GameObject bullet = Instantiate(bulletFactory);
+                bullet.transform.position = firePoint.position;
+            }
+
+            yield return new WaitForSeconds(delay);
+
         }
     }
 }
---
+++ MiniProject/Assets/Scripts/UI/AtkButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AtkButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public static AtkButton Instance;
+    public bool isTouch { get; set; }
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isTouch = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isTouch = false;
+    }
+}
